1f141702e1aa4b9b1dc62cb60e9f7ecabfa1e79b
Cameron Bateman <cameronbateman94@gmail.com>
2020-03-18 16:33:55 +0000

    update boundary colision
    changed boundary to alter velocirty to 0 when colliding

--- Assets/Circle.cs
+++ Assets/Circle.cs
@@ -2,18 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Circle : MonoBehaviour
 {
-
-    public float speed = 7;
-    Vector2 movement = new Vector2();
-
-    private Rigidbody2D rb2d;
-
+    
+    
     private float screenHalfWidthInWorldUnits;
     private float screenHalfHeightInWorldUnits;
-
-
     // Start is called before the first frame update
     void Start()
     {
@@ -21,48 +15,34 @@ public class PlayerController : MonoBehaviour
         float halfPlayerHeight = transform.localScale.y / 2f;
         screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize - halfPlayerWidth;
         screenHalfHeightInWorldUnits = Camera.main.orthographicSize - halfPlayerHeight;
-        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float inputX = Input.GetAxisRaw("Horizontal");
-        float inputY = Input.GetAxisRaw("Vertical");
-        Vector2 movement = new Vector2(inputX, inputY);
-        rb2d.AddForce(movement * speed);
-        Boundaries();
-
-
-
-
+     Boundaries();   
     }
-
+    
     void Boundaries()
     {
         if (transform.position.x < -screenHalfWidthInWorldUnits)
         {
             transform.position = new Vector2(-screenHalfWidthInWorldUnits, transform.position.y);
-            rb2d.velocity = new Vector2(0, 0);
         }
 
         if (transform.position.x > screenHalfWidthInWorldUnits)
         {
             transform.position = new Vector2(screenHalfWidthInWorldUnits, transform.position.y);
-            rb2d.velocity = new Vector2(0, 0);
         }
 
         if (transform.position.y < -screenHalfHeightInWorldUnits)
         {
-            transform.position = new Vector2(transform.position.x, -screenHalfHeightInWorldUnits);
-            rb2d.velocity = new Vector2(0, 0);
+            transform.position = new Vector2( transform.position.x, -screenHalfHeightInWorldUnits);
         }
 
         if (transform.position.y > screenHalfHeightInWorldUnits)
         {
             transform.position = new Vector2(transform.position.x, screenHalfHeightInWorldUnits);
-            rb2d.velocity = new Vector2(0, 0);
         }
     }
 }
-
