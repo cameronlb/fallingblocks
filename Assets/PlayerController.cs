91a23ad12f2431c4dd7abe9368afda297b203788
Cameron Bateman <cameronbateman94@gmail.com>
2020-03-18 11:22:12 +0000

    changed player movement
    changed player movement to use rigidbodyforce, added circle, added 2d collision to objects
    next: fix collision interaction

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,32 +7,57 @@ public class PlayerController : MonoBehaviour
 
     public float speed = 7;
 
+    private Rigidbody2D rb2d;
+
     private float screenHalfWidthInWorldUnits;
+    private float screenHalfHeightInWorldUnits;
+
+
     // Start is called before the first frame update
     void Start()
     {
         float halfPlayerWidth = transform.localScale.x / 2f;
+        float halfPlayerHeight = transform.localScale.y / 2f;
         screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize - halfPlayerWidth;
+        screenHalfHeightInWorldUnits = Camera.main.orthographicSize - halfPlayerHeight;
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float inputX = Input.GetAxisRaw("Horizontal");
-        float velocity = inputX * speed;
-        transform.Translate(Vector2.right * velocity * Time.deltaTime);
+        float inputY = Input.GetAxisRaw("Vertical");
+        Vector2 movement = new Vector2(inputX, inputY);
+        rb2d.AddForce(movement * speed);
+        Boundaries();
 
+
+        
+
+    }
+
+    void Boundaries()
+    {
         if (transform.position.x < -screenHalfWidthInWorldUnits)
         {
             transform.position = new Vector2(-screenHalfWidthInWorldUnits, transform.position.y);
         }
+
         if (transform.position.x > screenHalfWidthInWorldUnits)
         {
             transform.position = new Vector2(screenHalfWidthInWorldUnits, transform.position.y);
         }
 
+        if (transform.position.y < -screenHalfHeightInWorldUnits)
         {
-            
+            transform.position = new Vector2( transform.position.x, -screenHalfHeightInWorldUnits);
+        }
+
+        if (transform.position.y > screenHalfHeightInWorldUnits)
+        {
+            transform.position = new Vector2(transform.position.x, screenHalfHeightInWorldUnits);
         }
     }
 }
+
